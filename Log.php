4d7b367a8186242f5886672f528f73856b62de36
j-womack <womack.joshua@gmail.com>
2015-07-24 11:41:56 -0500

    Constructor/Destructor

--- Log.php
+++ Log.php
@@ -2,15 +2,24 @@
 class Log
 {
     public $fileName;
+    public $handle;
+    public function __construct($prefix = 'log') 
+    {
+        $this->fileName = "logs/$prefix-" . date('Y-m-d') . ".log";
+        $this->handle = fopen($this->fileName, 'a');
+    }
+
+    public function __destruct()
+    {
+        fclose($this->handle);
+    }
+
     public function message($logLevel, $message)
     {
         date_default_timezone_set('America/Chicago');
 
         $message = date('Y-m-d') . "   " . date('H:i:s') . "   [{$logLevel}] $message";
-
-        $handle = fopen($this->fileName, 'a');
-        fwrite($handle, $message . PHP_EOL);
-        fclose($handle);
+        fwrite($this->handle, $message . PHP_EOL);
     }
     public function info($info)
     {
--- log_test.php
+++ log_test.php
@@ -5,10 +5,10 @@ require_once 'Log.php';
 $date = date('Y-m-d');
 $log = new Log();
 
-$log->fileName = "logs/log-{$date}.log";
 $log->message("INFO", "This is an info message.");
 $log->info("Here's some info that I'm logging");
 $log->error("Here's an error!");
 
+unset($log);
 
 ?>
